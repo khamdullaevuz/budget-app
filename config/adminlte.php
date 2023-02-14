39068c9724200e485b2e5c3e5d4bd78e750ddc3f
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-02-14 23:46:09 +0500

    conf changed

--- config/adminlte.php
+++ config/adminlte.php
@@ -293,7 +293,7 @@ return [
         // Sidebar items:
         [
             'type' => 'sidebar-menu-search',
-            'text' => 'search',
+            'text' => 'izlash...g',
         ],
         [
           'text' => 'Bosh sahifa',
